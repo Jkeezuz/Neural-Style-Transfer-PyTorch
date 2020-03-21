2e48f2427fa2326b2deac046d5c316a1c51d0a1d
Daniel Matwicki <jkeezuz@gmail.com>
2020-03-21 19:37:01 +0100

    Add simple script to preprocess the images

--- dataprocess/resize.py
+++ dataprocess/resize.py
@@ -1,16 +1,24 @@
 import os
 
-from PIL import Image
-
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def resize(src_path, dest_path):
 
     for i, filename in enumerate(sorted(os.listdir(src_path))):
         # Convert image to RGB during opening
-        image = Image.open(filename).convert("RGB")
+        image = Image.open(os.path.join(src_path, filename)).convert("RGB")
 
         # Resize so we can load more to memory
-        image.resize((800, 800))
+        if image.size[0] > 800:
+            image = image.resize((800, image.size[1]))
+        if image.size[1] > 800:
+            image = image.resize((image.size[0], 800))
 
         # Save to dest path
         image.save(os.path.join(dest_path, filename))
+
+
+if __name__ == "__main__":
+
+    resize("../data/train/style", "../data/train/style_processed")
