353f4560bb80814fb02ba01a350b1d4d8835bf2f
Daniel Matwicki <jkeezuz@gmail.com>
2020-03-21 19:38:03 +0100

    Update paths to point to preprocessed folders

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ RESULTS_PATH = "images/results/"
 IMAGES_PATH = "images/"
 imsize = (300, 300)
 
-STYLE_PATH = "data/train/style"
-CONTENT_PATH = "data/train/content"
+STYLE_PATH = "data/train/style_processed"
+CONTENT_PATH = "data/train/content_processed"
