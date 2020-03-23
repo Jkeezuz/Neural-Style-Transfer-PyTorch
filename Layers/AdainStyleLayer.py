0acfeb3edf6555fad47d45d81d1bd8859053296a
Daniel Matwicki <jkeezuz@gmail.com>
2020-03-23 19:17:30 +0100

    nothing changed

--- Layers/AdainStyleLayer.py
+++ Layers/AdainStyleLayer.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+
 class AdainStyleLayer(nn.Module):
     """
     Custom style layer used to access the feature space
@@ -13,8 +14,13 @@ class AdainStyleLayer(nn.Module):
         super(AdainStyleLayer, self).__init__()
         # Compute the gram matrix of target activations for style image
         self.target_activations = None
+        self.with_loss = False
+        self.loss
 
     def forward(self, activations):
+
+        if self.with_loss:
+            pass
 
         self.target_activations = activations
         # Pass the activations forward in neural network
