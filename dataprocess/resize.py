8b3d08f4581ffb1a6724747f892ab83af8f673a6
Daniel Matwicki <jkeezuz@gmail.com>
2020-03-26 14:35:31 +0100

    change dimensions for std and mean

--- dataprocess/resize.py
+++ dataprocess/resize.py
@@ -3,6 +3,9 @@ import os
 from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+size = 300
+
+
 def resize(src_path, dest_path):
 
     for i, filename in enumerate(sorted(os.listdir(src_path))):
@@ -10,10 +13,10 @@ def resize(src_path, dest_path):
         image = Image.open(os.path.join(src_path, filename)).convert("RGB")
 
         # Resize so we can load more to memory
-        if image.size[0] > 800:
-            image = image.resize((800, image.size[1]))
-        if image.size[1] > 800:
-            image = image.resize((image.size[0], 800))
+        if image.size[0] > size:
+            image = image.resize((size, image.size[1]))
+        if image.size[1] > size:
+            image = image.resize((image.size[0], size))
 
         # Save to dest path
         image.save(os.path.join(dest_path, filename))
@@ -21,4 +24,4 @@ def resize(src_path, dest_path):
 
 if __name__ == "__main__":
 
-    resize("../data/train/style", "../data/train/style_processed")
+    resize("../data/train/content", "../data/train/content_processed")
