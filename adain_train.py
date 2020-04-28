c1defcc228e790ab87287805a5ea44418dc436f7
Daniel Matwicki <jkeezuz@gmail.com>
2020-04-28 14:46:40 +0200

    Update train script

--- adain_train.py
+++ adain_train.py
@@ -19,15 +19,17 @@ def rename(directory):
 # FOR TEST PURPOSES
 if __name__ == "__main__":
     # TRAIN
-    transformed_dataset = StyleTransferDataset(CONTENT_PATH, STYLE_PATH, transform=transforms.Compose([
-                                               transforms.Resize(256),
-                                               transforms.RandomCrop(224),
-                                               transforms.ToTensor()]))
-    dataloader = DataLoader(transformed_dataset, batch_size=8,
-                            shuffle=True, num_workers=4)
+    transformed_dataset = StyleTransferDataset(
+        CONTENT_PATH,
+        STYLE_PATH,
+        transform=transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomCrop(224),
+            transforms.ToTensor()]))
+    dataloader = DataLoader(transformed_dataset, batch_size=16,
+                            shuffle=True)
 
     adain = AdaIN()
-
     pprint.pprint(adain.encoder)
     pprint.pprint(adain.decoder)
 
@@ -35,8 +37,13 @@ if __name__ == "__main__":
     torch.save(adain.decoder.state_dict(), "decoder_random.pth")
 
     # Train with different style weights to see the difference
-    style_weights = [10e2, 10e3, 10e4]
+    import os
+    if not os.path.exists(RESULTS_PATH):
+        os.mkdir(RESULTS_PATH)
+    style_weights = [10e2]
     for sw in style_weights:
-        adain.train(dataloader=dataloader, style_weight=sw, epochs=5)
+        if not os.path.exists(os.path.join(RESULTS_PATH, "SW_{0}".format(sw))):
+            os.mkdir(os.path.join(RESULTS_PATH, "SW_{0}".format(sw)))
+        adain.train(dataloader=dataloader, style_weight=sw, epochs=25)
         # Reset decoder to starting weights
         adain.decoder.load_state_dict(torch.load("decoder_random.pth"))
